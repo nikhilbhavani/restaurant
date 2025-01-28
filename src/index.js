88f23dc509abb53d1711cf402e4c194c8fd6bad7
Nikhil Bhavani <86593547+nikhilbhavani@users.noreply.github.com>
2025-01-27 19:28:37 -0500

    little progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-import "./styles.css"
+import './styles.css'
+import home from './components/home/home.js'
+
+const navbut=document.querySelectorAll('nav-button');
+navbut.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        navbut.forEach((button)=>button.classList.remove('active'));
+        btn.classList.add('active');
+        switch(btn.id){
+            case 'home':
+                updateContent(home());
+                break;
+        }
+    });
+});
+
+function updateContent(component){
+    const contentDiv=document.getElementById('content');
+    contentDiv.textContent='';
+    contentDiv.appendChild(component);
+}
+
+updateContent(home());
